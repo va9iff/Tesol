161f31241a8312647598bbbdf29fa79368151701
va9iff <va9iff@gmail.com>
2023-02-04 06:35:00 +0400

    add marking answers, showing or hiding correct wrong

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
-let unique = ((a=0)=>()=>a++)()
-import {$} from '/query.js'
-import {el} from '/el.js'
+let unique = ((a = 0) => () => a++)()
+import { $ } from "/query.js"
+import { el } from "/el.js"
 let main = $.main
 
-
 let current = 0
-let questions = []
-let next = ()=> {
+let questions = [
+	{
+		asking: "Suallar yüklənir... Zəhmət olmasa gözləyin.",
+		answers: [],
+	},
+]
+let next = () => {
 	current++
-	if (current>=questions.length) current = questions.length - 1
+	if (current >= questions.length) current = questions.length - 1
 }
-let prev = ()=>{
+let prev = () => {
 	current--
 	if (current < 0) current = 0
 }
 
- async function getQuestions (){
-	let questionsText = await (await fetch('./questions.txt')).text()
+async function getQuestions() {
+	let questionsText = await (await fetch("./questions.txt")).text()
 	let questionsRaw = questionsText.split(/(?=#)/)
-	let questions = questionsRaw.map(questionRaw=>{
-		let question = {asking:"~va9iff", answers:[], correctId: 0}
+	let questions = questionsRaw.map(questionRaw => {
+		let question = { asking: "~va9iff", answers: [], correctId: 0 }
 		let components = questionRaw.split(/(?=[#\+\-])/)
-		for (let [i,component] of components.entries()) {
-			component = component.trim().replace('\r\n', '<br>')
-			if (component[0]=='#') {
+		for (let [i, component] of components.entries()) {
+			component = component.trim().replace("\r\n", "<br>")
+			if (component[0] == "#") {
 				question.asking = component.substr(1)
-			} else if (component[0]=='-') {
+			} else if (component[0] == "-") {
 				question.answers.push(component.substr(1))
-			}  else if (component[0]=='+') {
+			} else if (component[0] == "+") {
 				question.answers.push(component.substr(1))
 				question.correct = question.answers.length - 1
-			} 
+			}
 		}
 		return question
 	})
@@ -38,69 +42,85 @@ let prev = ()=>{
 }
 
 function createQuestion(question) {
-	let q = el('div.question')
-	q.setAttribute('s',unique())
+	let q = el("div.question")
+	q.setAttribute("s", unique())
 	q.innerHTML = `
 		<div class="asking">
-			${current}/${question.asking}
+			<i>${current}.</i> ${question.asking}
 		</div>
-		<div class="answers">
-			${question.answers.map(answer=>`<div class="answer">${answer}</div>`).join('')}
-		</div>
+		<div class="answers"></div>
 	`
+	question.answerElements = []
+	for (const [i, answer] of question.answers.entries()) {
+		let answerElement = el(`div.answer`)
+		question.answerElements.push(answerElement)
+		if (question.selected == i) answerElement.classList.add('selected')
+		answerElement.innerText = answer
+		answerElement.onclick = e => {
+			question.answerElements.forEach(ans=>ans.classList.remove('selected'))
+			if (question.correct == i) answerElement.classList.add('correct')
+			answerElement.classList.add('selected')
+			question.selected = i
+		}
+		q.querySelector(".answers").appendChild(answerElement)
+		console.log(question)
+	}
 	// console.log(q)
-	q.removeAfter = t=> setTimeout(()=>{
-		// console.log(q)
-		q.remove()
-	}, t)
+	q.removeAfter = t =>
+		setTimeout(() => {
+			// console.log(q)
+			q.remove()
+		}, t)
 	return q
 }
 
-let lastQuestion = 	createQuestion(
-		{
-			asking: `Suallarınızı rahatlıqla işləyə biləcəyiniz, və gözlərinizin hüzur tapacağı bir proje.`,
-			answers: [
-				`Cavablar isə aşağıda`,,
-				`Əl çatan`,
-				`Və ya barmaq çatan?`,
-				`nəysə söhbət çox uzanmasın.`,
-				`Uğurlar!`,
-				],
-			correct: 2}
-		)
+let lastQuestion = createQuestion({
+	asking: `Suallarınızı rahatlıqla işləyə biləcəyiniz, və gözlərinizin hüzur tapacağı bir proje.`,
+	answers: [
+		`Cavablar isə aşağıda`,
+		`Əl çatan`,
+		`Və ya barmaq çatan?`,
+		`nəysə söhbət çox uzanmasın.`,
+		`Uğurlar!`,
+	],
+	correct: 2,
+})
 
 $.question.replaceWith(lastQuestion)
-
-;(async ()=>{
+;(async () => {
 	questions = await getQuestions()
-	$.next.onclick = e=>{
+	$.next.onclick = e => {
 		next()
-		let q = createQuestion(questions[current])
+		let question = questions[current]
+		let q = createQuestion(question)
 
-		q.classList.add('fromRight')
+		q.classList.add("fromRight")
 
 		lastQuestion.after(q)
-		setTimeout(()=>lastQuestion.classList.remove('fromRight'), 40)
+		setTimeout(() => lastQuestion.classList.remove("fromRight"), 40)
 
-		lastQuestion.classList.add('fadeLeft')
+		lastQuestion.classList.add("fadeLeft")
 		lastQuestion.removeAfter(300)
 		lastQuestion = q
 	}
-	$.prev.onclick = e=>{
+	$.prev.onclick = e => {
 		prev()
 		let q = createQuestion(questions[current])
 		lastQuestion.after(q)
 
-		q.classList.add('fromLeft')
-		lastQuestion.classList.add('fadeRight')
+		q.classList.add("fromLeft")
+		lastQuestion.classList.add("fadeRight")
 
 		lastQuestion.after(q)
-		setTimeout(()=>lastQuestion.classList.remove('fromLeft'), 40)
+		setTimeout(() => lastQuestion.classList.remove("fromLeft"), 40)
 
 		lastQuestion.removeAfter(300)
 		lastQuestion = q
-
 	}
 })()
 
 
+$.eye.onclick = ()=>{
+	console.log(main)
+	main.classList.toggle('eyeOpen')
+}
