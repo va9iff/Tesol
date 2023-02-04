ff64e04910dee6870895e22e7a9cfac71322de39
va9iff <va9iff@gmail.com>
2023-02-05 00:49:28 +0400

    full with icons; but icons looks old fashion

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 let unique = ((a = 0) => () => a++)()
-import { $ } from "/query.js"
-import { el } from "/el.js"
+const $ = new Proxy({}, { get: (_, c) => document.querySelector(`.${c}`) })
+const el = str =>{
+	const strs = str.split('.')
+	let el = document.createElement(strs.shift())
+	for (let cls of strs) el.classList.add(cls)
+	return el
+}
+
+el('div.wrapper.hidden')
 let main = $.main
 
-let current = 0
-let questions = [
-	{
-		asking: "Suallar yüklənir... Zəhmət olmasa gözləyin.",
-		answers: [],
-	},
-]
-let next = () => {
-	current++
-	if (current >= questions.length) current = questions.length - 1
-}
-let prev = () => {
-	current--
-	if (current < 0) current = 0
-}
+let current = -1
+let questions = []
 
 async function getQuestions() {
 	let questionsText = await (await fetch("./questions.txt")).text()
@@ -25,7 +19,7 @@ async function getQuestions() {
 	let questions = questionsRaw.map(questionRaw => {
 		let question = { asking: "~va9iff", answers: [], correctId: 0 }
 		let components = questionRaw.split(/(?=(?:\n#)|(?:\n\+)|(?:\n-))/)
-		console.log(components)
+		// console.log(components)
 		for (let [i, component] of components.entries()) {
 			component = component.trim().replace("\r\n", "<br>")
 			if (component[0] == "#") {
@@ -42,15 +36,39 @@ async function getQuestions() {
 	return questions
 }
 
+
+function goTo(num, direction = true /*true for next, flase for prev*/) {
+	console.log(num)
+	num = +num
+	let question = questions[num]
+	question.num = num
+	let q = createQuestion(question)
+
+	q.classList.add(direction ? "fromRight" : "fromLeft")
+
+	lastQuestion.after(q)
+		setTimeout(() => lastQuestion.classList.remove(direction ? "fromRight" : "fromLeft"), 40)
+		lastQuestion.classList.add(direction ? "fadeLeft" : "fadeRight")
+	lastQuestion.removeAfter(300)
+	lastQuestion = q
+	current = num
+}
+
+// setTimeout(()=>goTo(9),500)
+
 function createQuestion(question) {
 	let q = el("div.question")
 	q.setAttribute("s", unique())
+	console.log(question)
 	q.innerHTML = `
 		<div class="asking">
-			<i>${current}.</i> ${question.asking}
+			<i>${question.num ? +question.num+1 : current+1}.</i> ${question.asking}
 		</div>
 		<div class="answers"></div>
 	`
+	// q.querySelector('i').onclick = e=> {
+	// 	e.target.after(createGoToDial(question.num))
+	// }
 	question.answerElements = []
 	for (const [i, answer] of question.answers.entries()) {
 		let answerElement = el(`div.answer`)
@@ -98,50 +116,69 @@ let lastQuestion = createQuestion({
 })
 
 $.question.replaceWith(lastQuestion)
+
 ;(async () => {
 	questions = await getQuestions()
+	gotoNum.max = +questions.length	
 	$.next.onclick = e => {
-		if (current == questions.length - 1) return	
-		next()
-		let question = questions[current]
-		let q = createQuestion(question)
-
-		q.classList.add("fromRight")
-
-		lastQuestion.after(q)
-		setTimeout(() => lastQuestion.classList.remove("fromRight"), 40)
-
-		lastQuestion.classList.add("fadeLeft")
-		lastQuestion.removeAfter(300)
-		lastQuestion = q
+		if (current >= questions.length - 1) {
+			current = questions.length - 1
+			return
+		} else
+			current ++
+		goTo(current)
 	}
 	$.prev.onclick = e => {
-		if (current == 0) return	
-		prev()
-		let q = createQuestion(questions[current])
-		lastQuestion.after(q)
-
-		q.classList.add("fromLeft")
-		lastQuestion.classList.add("fadeRight")
-
-		lastQuestion.after(q)
-		setTimeout(() => lastQuestion.classList.remove("fromLeft"), 40)
-
-		lastQuestion.removeAfter(300)
-		lastQuestion = q
+		if (current <=  0) {
+			current = 0
+			return
+		} else
+			current--
+		goTo(current, false)
 	}
 })()
 
 
+let norms = [$.fullScreen, $.eye, $.pull, $.goto]
+let hides = [$.gotoNum]
+
+function norm() {
+	norms.forEach(a=>a.classList.remove('hidden'))
+	hides.forEach(hide=>{
+		hide.classList.add('hidden')
+		hide.visible = false
+	})
+}
+function spec(...specs) {
+	[...norms, ...hides].forEach(a=>a.classList.add('hidden'))
+	;[...specs].forEach(spec=>{
+		spec.classList.remove('hidden')
+		spec.visible = true
+	})
+}
+
+
 $.eye.onclick = e=>{
-	console.log(main)
 	e.target.classList.toggle('highlight')
 	main.classList.toggle('eyeOpen')
 }
 
 $.pull.onclick = e=>{
 	main.classList.toggle('pulled')
-	e.target.classList.toggle('pulled')
+	// e.target.classList.toggle('pulled')
 }
 
 $.fullScreen.onclick = e => document.body.requestFullscreen()
+
+const gotoNum = $.gotoNum
+$.goto.onclick = e=> {
+	if (!gotoNum.visible) {
+		spec($.goto, $.gotoNum)
+
+		return null
+	}
+	goTo(+gotoNum.value-1)
+	gotoNum.classList.add('hidden')
+	norm()
+}
+gotoNum.onchange = $.goto.onclick
